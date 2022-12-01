e0ced25245d6d34708622061f1f5638eb80666b0
0xc0c0 <0xc0c0@benslab.org>
2022-11-30 23:26:52 -0600

    Cleaned up Day 1

--- day1/test_solve.py
+++ day1/test_solve.py
@@ -8,11 +8,13 @@ def test_data():
     return text
 
 def test_parse_input(test_data):
-    grid = parse_data(test_data)
-    assert type(grid) == np.ndarray
-    assert grid[1,3] == 5
-    assert grid.ndim == 2
-    assert grid.shape == (10,10)
-  
+    inventories = parse_data(test_data)
+    assert type(inventories) == list
+    assert inventories[0][0] == 1000
+    assert len(inventories) == 5
+    assert inventories[3][1] == 8000
+      
 def test_all(test_data):
-    grid = parse_data(test_data)
+    inventories = parse_data(test_data)
+    assert get_max_inventory(inventories) == 24000
+    assert get_top3_inventories(inventories) == 45000

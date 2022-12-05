3c38956f4d285025de9f3af1443375b1b29d9a74
0xc0c0 <0xc0c0@benslab.org>
2022-12-05 00:16:01 -0600

    WIP start of day 6

---
+++ day6/test_solve.py
@@ -0,0 +1,17 @@
+import pytest
+import os
+from .solve import *
+
+@pytest.fixture
+def test_data():
+    #dynamically obtain full path of 'test.txt'
+    test_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'test.txt')
+    with open(test_file, 'r') as f:
+        text = f.read()
+    return text
+
+def test_parse_input(test_data):
+    grid = parse_data(test_data)
+  
+def test_all(test_data):
+    grid = parse_data(test_data)

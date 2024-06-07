cb1f5a8c4a9f4dd08b18335cf9cfdfe5e7a48606
0xc0c0 <c0c0@benslab.org>
2024-06-07 18:16:52 -0500

    WIP

--- day16/solve.py
+++ day16/solve.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 from typing import List, Dict
-from copy import copy,deepcopy
+from copy import copy, deepcopy
 import logging
 import os
 import re
@@ -10,105 +10,98 @@ import numpy as np
 logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
 logger = logging.getLogger(__name__)
 
-@dataclass
-class FlowNode:
-    name: str
-    rate: int
-    next_nodes: List[FlowNode]
-    
-    def __hash__(self):
-        return hash(self.name)
-    
-    def __repr__(self):
-        return f"name: {self.name}, rate: {self.rate}, next_nodes: {[n.name for n in self.next_nodes]}"
-    
-def get_file_data(fn='input.txt'):
-    with open(fn) as f:
+
+# @dataclass
+# class FlowNode:
+#     """Data class for the nodes in the volcano flow control system for Day 16 AoC 2022"""
+
+#     name: str
+#     rate: int
+#     next_nodes: List[FlowNode]
+
+#     def __hash__(self):
+#         return hash(self.name)
+
+#     def __repr__(self):
+#         next_nodes = [n.name for n in self.next_nodes]
+#         return f"{self.name}, {self.rate}, next: {[next_nodes]}"
+
+
+def get_file_data(fn="input.txt"):
+    """Get file data from input.txt
+
+    Args:
+        fn (str, optional): _description_. Defaults to "input.txt".
+
+    Returns:
+        _type_: raw data from the file as a str
+    """
+    with open(fn, "r", encoding="utf-8") as f:
         data = f.read()
     return data
 
+
 def parse_line(text_line):
-    first_text, second_text = text_line.split(';')
-    first_regex = 'Valve ([A-Z]{2}) has flow rate=([0-9]+)'
-    second_regex = '[A-Z]{2}'
+    """Parse a line of text
+
+    Args:
+        text_line (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    first_text, second_text = text_line.split(";")
+    first_regex = "Valve ([A-Z]{2}) has flow rate=([0-9]+)"
+    second_regex = "[A-Z]{2}"
     name, rate_text = re.findall(first_regex, first_text)[0]
     next_nodes = re.findall(second_regex, second_text)
     return name, int(rate_text), next_nodes
 
+
 def parse_data(text_data):
-    entries = [parse_line(line) for line in text_data.strip('\n').strip().split('\n')]
-    all_nodes = dict()
-    for name, rate, _ in entries:
-        all_nodes[name] = FlowNode(name, rate, list())
-    for name, _, next_nodes in entries:
-        for n in next_nodes:
-            all_nodes[name].next_nodes.append(all_nodes[n])
-    return all_nodes
+    """Parse all the data from the input
 
-def get_useful_remaining_valves(states):
-    return [n for n,v in states.items() if n.rate > 0 and v == False]
+    Args:
+        text_data (_type_): _description_
 
-def init_states(all_nodes:Dict[str]):
-    return {v:False for (k,v) in all_nodes.items() if v.rate != 0}
+    Returns:
+        _type_: _description_
+    """
+    entries = [parse_line(line) for line in text_data.strip("\n").strip().split("\n")]
+    flow_nodes = {n: {"r": r, "nn": nn} for (n, r, nn) in entries}
 
-def get_states_node_tracker_index(states, node:FlowNode):
-    return [1 if v else 0 for _,v in states.items()] + [node.name]
+    return flow_nodes
 
-def get_states_node_tracker(states_node_tracker, states, node:FlowNode):
-    i = get_states_node_tracker_index(states, node)
-    if i in states_node_tracker:
-        return states_node_tracker[i]
+TRACKER = dict()
+
+def get_max_pressure_released(flow_nodes, cur_node='AA', opened_nodes=tuple(), min_left=30, released=0):
+    """Find max possible pressure releasable from flow nodes
+
+    Args:
+        flow_nodes (list): list of flow nodes and their tunneling paths
+    """
+    if min_left == 0:
+        if min_left >= TRACKER[(cur_node, opened_nodes)]['min_left'] and released
+        return released
+    #add check for memoization here
+    elif 
     else:
-        return None
-
-def update_states_node_tracker(states_node_tracker, states, node, time_left, value):
-    i = get_states_node_tracker_index(states, node)
-    states_node_tracker[i] = value
-
-def get_optimal_pressure(node:FlowNode, states, states_node_tracker, released_pressure:int=0, time_left:int=30):
-    # initialize states tracker
-    # [states + value_found]
-    preexisting_value = get_states_node_tracker(states, node, time_left)
-    if preexisting_value:
-        return preexisting_value
-    
-    # check if all useful valves are already open
-    logger.debug(f"node: {node.name}, time left: {time_left}")
-    states_check = get_useful_remaining_valves(states)
-    if len(states_check) == 0:  #nothing left to do
-        return 0
-    elif time_left <= 1:  #nothing can be value added with 1 minutes left (opening a valve still results in 0 * rate)
-        return 0
-    else:
-        # need to compare stopping and opening, then moving on
-        # to moving to each next value and calculating their downstream optimals
-        comparisons = list()
-        
-        #account for turning this state on
-        if node in states_check:
-            next_states = copy(states)
-            next_states[node] = True
-            released_pressure = node.rate * (time_left - 1)
-            comparisons.append(released_pressure + get_optimal_pressure(node, next_states, time_left - 1))
-        
-        #now account for moving to any next node
-        for nn in node.next_nodes:
-            comparisons.append(get_optimal_pressure(nn, states, time_left - 1))
-        if states_node_tracker[(states,node)] < max(comparisons):
-            states_node_tracker[(states,node)] = max(comparisons)
-        
-        return states_node_tracker[states]
-
+        option1 = None
+        if flow_nodes[cur_node]['r'] > 0:
+            option1 = flow_nodes
 
 
 def main():
+    """Complete Day 16 problems"""
     logger.setLevel(level=logging.INFO)
     data = get_file_data()
-    model = parse_data(data)
+    flow_nodes = parse_data(data)
+
     answer = 0
     print(f"Puzzle1: <SUMMARY>: {answer}")
     answer = 0
     print(f"Puzzle2: <SUMMARY>: {answer}")
-    
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     main()
--- day16/test_solve.py
+++ day16/test_solve.py
@@ -1,27 +1,44 @@
-import pytest
+"""Test program using pytest for AoC solver"""
 import os
-from .solve import *
+import pytest
+from .solve import parse_data, init_states, get_optimal_pressure
 
-@pytest.fixture
-def test_data():
-    #dynamically obtain full path of 'test.txt'
-    test_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'test.txt')
-    with open(test_file, 'r') as f:
+
+@pytest.fixture(name="test_data")
+def get_test_data():
+    """Data fixture for AoC test data
+
+    Returns:
+        str: raw test data as a string
+    """
+    # dynamically obtain full path of 'test.txt'
+    test_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), "test.txt")
+    with open(test_file, "r", encoding="utf-8") as f:
         text = f.read()
     return text
 
+
 def test_parse_input(test_data):
-    nodes = parse_data(test_data)
-    assert len(nodes) == 10
-    assert nodes['BB'].rate == 13
-    assert nodes['GG'].rate == 0
-    ff = nodes['FF']
-    assert ff in nodes['GG'].next_nodes
-    states = init_states(nodes)
-    assert states[ff] == False
-  
+    """Tests parsing functions for test data
+
+    Returns:
+        str: raw test data as a string
+    """
+    flow_nodes = parse_data(test_data)
+    assert len(flow_nodes) == 10
+    assert flow_nodes["BB"]["r"] == 13
+    assert flow_nodes["GG"]["r"] == 0
+    assert "FF" in flow_nodes["GG"]["nn"]
+
+
 def test_all(test_data):
-    nodes = parse_data(test_data)
-    states = init_states(nodes)
-    pressure = get_optimal_pressure(nodes['AA'], states, 30)
-    assert pressure == 1651
+    """Test cases for actual problem
+
+    Returns:
+        str: raw test data as a string
+    """
+    pass
+    # nodes = parse_data(test_data)
+    # states = init_states(nodes)
+    # pressure = get_optimal_pressure(nodes["AA"], states, 30)
+    # assert pressure == 1651
